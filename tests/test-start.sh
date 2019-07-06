fff20493e38b394ab0ae73f8644b9941c2393c51
Karim Alibhai <karim@alibhai.co>
2019-07-06 04:06:15 -0400

    Add: tests for start

---
+++ tests/test-start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+cd "`dirname $0`/test-start"
+wd="$PWD"
+
+source "$wd/../helpers.sh"
+
+log="`mktemp`"
+
+function printlog() {
+    echo '======== start output log ========'
+    cat $log
+    echo '========  end output log  ========'
+    echo
+}
+
+if ! "$wd/../../mono.sh" "start" &> "$log"; then
+    printlog
+    print_err "Failed to run: mono start"
+fi
+
+if ! fgrep "[a]: hello from a" "$log" >/dev/null; then
+    printlog
+    print_err "Expected to find '[a]: hello from a' in logfile"
+elif ! fgrep "[b]: hello from b" "$log" >/dev/null; then
+    printlog
+    print_err "Expected to find '[b]: hello from b' in logfile"
+fi
