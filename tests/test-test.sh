dc8d165a2cd328cb2846f5f27bec54ed5289304c
Karim Alibhai <karim@alibhai.co>
2019-07-06 04:06:22 -0400

    Add: tests for test

---
+++ tests/test-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+cd "`dirname $0`/test-test"
+wd="$PWD"
+
+source "$wd/../helpers.sh"
+
+rm -rf \
+    "$wd/pretest-ran" \
+    "$wd/test-ran" \
+    "$wd/posttest-ran" \
+    "$wd/packages/apple/pretest-ran" \
+    "$wd/packages/apple/test-ran" \
+    "$wd/packages/apple/posttest-ran" \
+    "$wd/packages/baseball/pretest-ran" \
+    "$wd/packages/baseball/test-ran" \
+    "$wd/packages/baseball/posttest-ran"
+
+not_exists "$wd/pretest-ran"
+not_exists "$wd/test-ran"
+not_exists "$wd/posttest-ran"
+not_exists "$wd/packages/apple/pretest-ran"
+not_exists "$wd/packages/apple/test-ran"
+not_exists "$wd/packages/apple/posttest-ran"
+not_exists "$wd/packages/baseball/pretest-ran"
+not_exists "$wd/packages/baseball/test-ran"
+not_exists "$wd/packages/baseball/posttest-ran"
+
+"$wd/../../mono.sh" "test"
+
+exists "$wd/pretest-ran"
+not_exists "$wd/test-ran" "Should still not exist, since there is no point of running 'npm test' in the root"
+exists "$wd/posttest-ran"
+exists "$wd/packages/apple/pretest-ran"
+exists "$wd/packages/apple/test-ran"
+exists "$wd/packages/apple/posttest-ran"
+exists "$wd/packages/baseball/pretest-ran"
+exists "$wd/packages/baseball/test-ran"
+exists "$wd/packages/baseball/posttest-ran"
