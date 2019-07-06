9e384f53a824cc6d68bf9c4ffc5825996839976d
Karim Alibhai <karim@alibhai.co>
2019-07-06 04:19:39 -0400

    Add: tests for overriding order

---
+++ tests/test-run-order.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+cd "`dirname $0`/test-run-order"
+wd="$PWD"
+
+source "$wd/../helpers.sh"
+
+rm -rf \
+    "$wd/run-order"
+
+not_exists "$wd/run-order"
+
+"$wd/../../mono.sh" run build
+
+exists "$wd/run-order"
+
+order="`cat $wd/run-order | tr -d '\n'`"
+if test "$order" != "cba"; then
+    print_err "Expected execution order C-B-A, but got: $order"
+fi
